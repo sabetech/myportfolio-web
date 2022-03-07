20f9dc5eb02bcb62982b341f53b07efcceab564f
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-03-07 20:09:15 +0500

    updated js and html file

--- portforlio.js
+++ portforlio.js
@@ -2,6 +2,8 @@ const sideMenuButton = document.getElementById('side-menu-button');
 const sideMenuModal = document.getElementById('mobile-menu-modal');
 const mainPage = document.getElementById('main-page-content');
 const closemodal = document.querySelector('.close-modal')
+const menuItems = document.querySelectorAll('.mobile-menu-list-item');
+
 let isVisible = false;
 
 
@@ -18,3 +20,10 @@ closemodal.addEventListener('click', () =>{
   toggleModalVisibility();
 });
 
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', (ev) => {
+    toggleModalVisibility();
+    window.location.hash = ev.target.getAttribute('data-location');
+  });
+});
