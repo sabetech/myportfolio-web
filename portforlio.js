871dae3e176d5682ab8d2915d6758faf2e99086d
Albert <albert.mensahansah@gmail.com>
2022-03-07 15:56:29 +0000

    Fix linter issues

--- portforlio.js
+++ portforlio.js
@@ -1,11 +1,10 @@
 const sideMenuButton = document.getElementById('side-menu-button');
 const sideMenuModal = document.getElementById('mobile-menu-modal');
 const mainPage = document.getElementById('main-page-content');
-const closemodal = document.querySelector('.close-modal')
+const closemodal = document.querySelector('.close-modal');
 const menuItems = document.querySelectorAll('.mobile-menu-list-item');
 
 let isVisible = false;
-
 
 function toggleModalVisibility() {
   isVisible = !isVisible;
@@ -16,10 +15,9 @@ function toggleModalVisibility() {
 sideMenuButton.addEventListener('click', () => {
   toggleModalVisibility();
 });
-closemodal.addEventListener('click', () =>{
+closemodal.addEventListener('click', () => {
   toggleModalVisibility();
 });
-
 
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', (ev) => {
