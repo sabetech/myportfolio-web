beb4f4359d8a595463e2c5a4db01e2621ba47a21
Albert <albert.mensahansah@gmail.com>
2022-03-07 13:35:37 +0000

    Fix js linter errors

--- portforlio.js
+++ portforlio.js
@@ -1,16 +1,14 @@
-let sideMenuButton = document.getElementById("side-menu-button");
-let sideMenuModal = document.getElementById("mobile-menu-modal");
-let mainPage = document.getElementById("main-page-content");
+const sideMenuButton = document.getElementById('side-menu-button');
+const sideMenuModal = document.getElementById('mobile-menu-modal');
+const mainPage = document.getElementById('main-page-content');
 let isVisible = false;
 
-sideMenuButton.addEventListener('click', function (ev) {
-
-    toggleModalVisibility();
-
-});
-
-function toggleModalVisibility(){
-    isVisible = !isVisible;
-    sideMenuModal.classList.toggle('is-visible');
-    mainPage.style.display = isVisible ? 'none':'block';
+function toggleModalVisibility() {
+  isVisible = !isVisible;
+  sideMenuModal.classList.toggle('is-visible');
+  mainPage.style.display = isVisible ? 'none' : 'block';
 }
+
+sideMenuButton.addEventListener('click', () => {
+  toggleModalVisibility();
+});
