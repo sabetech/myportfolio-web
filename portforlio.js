e58699900d431b53a0475b3cf0986708e601128f
Albert <albert.mensahansah@gmail.com>
2022-03-07 13:23:50 +0000

    Show side mneu modal on hamburgar click with js

--- portforlio.js
+++ portforlio.js
@@ -1,7 +1,16 @@
 let sideMenuButton = document.getElementById("side-menu-button");
+let sideMenuModal = document.getElementById("mobile-menu-modal");
+let mainPage = document.getElementById("main-page-content");
+let isVisible = false;
 
 sideMenuButton.addEventListener('click', function (ev) {
 
-    
+    toggleModalVisibility();
 
 });
+
+function toggleModalVisibility(){
+    isVisible = !isVisible;
+    sideMenuModal.classList.toggle('is-visible');
+    mainPage.style.display = isVisible ? 'none':'block';
+}
